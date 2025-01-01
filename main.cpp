36d19b61abf6a23eb35e1ea46f9e8c13591e8003
alinapavlyuk <alino4kap12@gmail.com>
2025-01-01 20:25:06 +0900

    test: add performance test for build_index func
    -test measures how much time it takes to build index depending on number of threads

--- main.cpp
+++ main.cpp
@@ -3,11 +3,36 @@
 
 #define EXECUTORS_NUM 6
 
+void test() {
+    std::vector<int> threads_nums = {1, 2, 4, 6, 8, 16, 32};
+
+    for (const auto threads_num: threads_nums) {
+        ThreadPool thread_pool;
+        thread_pool.initialize(EXECUTORS_NUM);
+
+        FileManager file_manager("../data", &thread_pool, 10000);
+
+        printf("Time taken to build index:\n");
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        file_manager.build_index();
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        printf("With %d threads: %lld milliseconds\n", threads_num, duration.count());
+        thread_pool.terminate();
+    }
+}
+
+
 int main() {
+//    test();
     ThreadPool thread_pool;
     thread_pool.initialize(EXECUTORS_NUM);
 
-    FileManager file_manager("../data", &thread_pool);
+    FileManager file_manager("../data", &thread_pool, 10000);
     file_manager.build_index();
     file_manager.assign_file_watcher();
 
